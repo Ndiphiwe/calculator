9a7e6cddbe14622e8882d6f455c2c073dbe761e1
Ndiphiwe Ngxola <pndira1770@gmail.com>
2021-11-24 12:20:03 +0200

    added files

---
+++ calculator.js
@@ -0,0 +1,27 @@
+let sum = '';
+
+function addToSum(operator){
+    sum = sum + operator;
+    return document.getElementById("myOutput").innerText = sum;
+}
+
+function evaluateSum(){
+    document.getElementById("history").innerText = sum;
+    sum = eval(sum);
+    document.getElementById("myOutput").innerText = sum;
+}
+
+function clearSum(){
+    sum = '';
+    document.getElementById("myOutput").innerText = sum;
+    document.getElementById("history").innerText = sum;
+}
+
+function deleteNum(){
+    sum = sum.slice(0,-1);
+    document.getElementById("myOutput").innerText = sum;
+}
+
+let e = '2*2.3';
+// let ans  document.getElementById("myOutput").innerText;
+console.log(eval(e));
---
+++ index.js
@@ -0,0 +1,56 @@
+//my first console
+//console.log('Hello world');
+
+// let myName = 'Biggus Dickus';
+// console.log(myName);
+
+// let firstName;
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let myName = 'Biggus Dickus'; //String Literal
+let age = 20; //Number Literal
+let inFull = 'is';
+let inFullTwo = 'years old';
+let isApproved = true; // Boolean literal
+let firstName = undefined;
+let selectColor = null;
+
+console.log(myName, inFull, age, inFullTwo);
+
+ 
+//object
+let persin = {
+    theName: 'Yeehaao',
+    theAge: 22,
+
+};
+console.log(persin);
+
+//Dot Notation
+// persin.theName = 'Yee';
+// console.log(persin.theName);
+
+//Bracket notation
+// persin['theName'] = 'Hooha';
+
+// console.log(myName, 'is ' + age + ' years old');
+
+
+
+let selectedColors = ['green', 'blue'];
+selectedColors [2] = 'yellow';
+// console.log(selectedColors.length);
+console.log(selectedColors);
+
+
+function calcu(num){
+    return num * num;
+}
+console.log(calcu(4));
+
+let sum = '1 + 2';
+console.log(sum);
+console.log(eval(sum));
